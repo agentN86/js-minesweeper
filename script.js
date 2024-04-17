441232203b3c5f114565f3db2ff03eb47f46b4c8
agentN86 <128328445+agentN86@users.noreply.github.com>
2024-04-17 18:16:26 -0500

    working table close/open now (hopefully)

--- script.js
+++ script.js
@@ -65,6 +65,19 @@ if (realdigits.length == 1) {
 }
 
 
+document.getElementById('gamesBTN').onclick = function() {
+	var elements = document.getElementsByClassName('gamesTableTH')
+	if (elements[0].style.display === "table-row") {
+	  for (let i = 0; i < elements.length; i++) {
+		elements[i].style.display = "none"
+	  }
+	} else {
+	  for (let i = 0; i < elements.length; i++) {
+		elements[i].style.display = "table-row"
+	  }
+	}
+  }
+
 
 const gameBoard =
 	document.getElementById(
